2db0ba0117f76eef9202149cc0fa722700410cce
wesamAlsaleh <wesammuneer7@gmail.com>
2025-12-28 18:42:28 +0300

    feat: add the test api endpoint as public endoint

--- src/main/java/com/generalassembly/todo/configs/SecurityConfig.java
+++ src/main/java/com/generalassembly/todo/configs/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(request ->
                 request
                         // Public endpoints (no authentication required)
+                        .requestMatchers(HttpMethod.GET, "/auth/").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/logout").permitAll()
